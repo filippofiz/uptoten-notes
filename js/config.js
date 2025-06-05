17c4e7244ae76d8723007363af8952fe0b0b7e7f
filippofiz <filippo.fiz@uptoten.it>
2025-06-05 14:41:45 +0200

    Fix Supabase configuration

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Configurazione Supabase
-const SUPABASE_URL = 'https://cuuqdfrjqprdpvkvrvws.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1dXFkZnJqcXByZHB2a3ZydndzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4NDk0ODAsImV4cCI6MjA2NDQyNTQ4MH0.72ZHZFBcU4h76bvFJRqYijcS_GeZCsbtQWnWutIgd8A';
+const SUPABASE_URL = 'https://cuuqdfrjqprdpvkyrvws.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1dXFkZnJqcXByZHB2a3lydndzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg4NDk0ODAsImV4cCI6MjA2NDQyNTQ4MH0.72ZHZFBcU4h76bvFJRqYijcS_GeZCsbtQWnWutIgd8A';
 
 // Variabili globali
 let supabase = null;
